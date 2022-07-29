f6fb84d3197ef92ba1b73d24d93ae0b5c0b3cc79
John Gorkos - AB0OO <jgorkos@gmail.com>
2022-07-28 17:56:40 -0700

    Accuracy rewrite.  Center camp is still a little wonky.

--- gen_brc_shapefile.py
+++ gen_brc_shapefile.py
@@ -1,26 +1,82 @@
 import geopy.distance
 import fiona
+import math
 
-degOffset=45
-streetNames = ['Esplanade','A','B','C','D','E','F','G','H','I','J','K' ]
+# this will change every year for BM
+goldenSpike = [40.78634966315868, -119.20651954500156]
 streetDepths = [2500,440,290,290,290,290,490,290,290,290,190,190]
-radialNames = ['2:00','2:30','3:00','3:30','4:00','4:30','5:00','5:30','6:00', \
-               '6:30','7:00','7:30','8:00','8:30','9:00','9:30','10:00' ]
-shortRadialNames = ['2:15','2:45','3:15','3:45','4:15','4:45','5:15','5:45','6:15', \
-                    '6:45','7:15','7:45','8:15','8:45','9:15','9:45' ]
+distToCenterCamp = 2900
+manRingRadius = 250
+templeRingRadius = 125
+centerCampPlazaRadius = 300
+rodsRingRadius = 760
+scale = 2
+
+# nothing below here should normally change.
+degOffset=45
+streets = { 'Esplanade': 2500,
+            'A': 2940,
+            'B': 3230,
+            'C': 3520,
+            'D': 3810,
+            'E': 4100,
+            'F': 4590,
+            'G': 4880,
+            'H': 5170,
+            'I': 5460,
+            'J': 5650,
+            'K': 5840 }
+
+radials = { '2:00': 60.0,
+            '2:15': 67.5,
+            '2:30': 75.0,
+            '2:45': 82.5,
+            '3:00': 90.0,
+            '3:15': 97.5,
+            '3:30': 105.0,
+            '3:45': 112.5,
+            '4:00': 120.0,
+            '4:!5': 127.5,
+            '4:30': 135.0,
+            '4:45': 142.5,
+            '5:00': 150.0,
+            '5:15': 157.5,
+            '5:30': 165.0,
+            '5:45': 172.5,
+            '6:00': 180.0,
+            '6:15': 187.5,
+            '6:30': 195.0,
+            '6:45': 202.5,
+            '7:00': 210.0,
+            '7:15': 217.5,
+            '7:30': 225.0,
+            '7:45': 232.5,
+            '8:00': 240.0,
+            '8:15': 247.5,
+            '8:30': 255.0,
+            '8:45': 262.5,
+            '9:00': 270.0,
+            '9:15': 277.5,
+            '9:30': 285.0,
+            '9:45': 292.5,
+            '10:00': 300 }
+
+def getStreetByDeg( deg:int ) -> str:
+    for k,v in radials.items():
+        if deg==v:
+            return k
+
+cc = geopy.distance.distance(feet=distToCenterCamp).destination(goldenSpike, bearing=degOffset+180)
+centerCamp = [cc.latitude,cc.longitude]
+tm = geopy.distance.distance(feet=streets['Esplanade']).destination(goldenSpike, bearing=degOffset)
+temple = [tm.latitude,tm.longitude]
+
                 
-currDist = 0;
-
-maxDistance = sum(streetDepths)
-
 # define schema
 schema = {
     'geometry':'LineString',
     'properties':[('Name','str')]
 }
-goldenSpike = [40.78634966315868, -119.20651954500156]
-cc = geopy.distance.distance(feet=2925).destination(goldenSpike, bearing=degOffset+180)
-centerCamp = [cc.latitude,cc.longitude]
 
 #open a fiona object
 lineShp = fiona.open('bm_2019.shp', mode='w', driver='ESRI Shapefile',
@@ -28,70 +84,157 @@ lineShp = fiona.open('bm_2019.shp', mode='w', driver='ESRI Shapefile',
 
 # annular streets
 idx=0
-for s in streetDepths:
-    currDist = currDist+s
-    print(f"Distance for center of {streetNames[idx]} is {currDist}")
-    radial = []
-    for d in range(degOffset+60,degOffset+301,5):
-        foo = geopy.distance.distance(feet=currDist).destination(goldenSpike, bearing=d)
-        radial.append((foo.longitude,foo.latitude))
+for name,distance in streets.items():
+    annular = []
+    for d in range((degOffset+60)*scale,(degOffset+300)*scale+1,1):
+        foo = geopy.distance.distance(feet=distance).destination(goldenSpike, bearing=d/scale)
+        if geopy.distance.distance(foo,centerCamp).feet >= rodsRingRadius: 
+            annular.append((foo.longitude,foo.latitude))
+        else:
+            rowDict = {
+                    'geometry': {'type':'LineString',
+                                'coordinates':annular},
+                    'properties': {'Name': name,
+                        }
+                    }
+            lineShp.write(rowDict)
+            annular = []
     
     rowDict = {
             'geometry': {'type':'LineString',
-                        'coordinates':radial},
-            'properties': {'Name': streetNames[idx],
+                        'coordinates':annular},
+            'properties': {'Name': name,
                 }
             }
     lineShp.write(rowDict)
-    idx=idx+1
 
 # radial streets
-idx=0
-for r in range(degOffset+60,degOffset+301,15):
-    street = []
-    foo = geopy.distance.distance(feet=2500).destination(goldenSpike, bearing=r)
-    street.append((foo.longitude,foo.latitude))
-    foo = geopy.distance.distance(feet=maxDistance).destination(goldenSpike, bearing=r)
-    street.append((foo.longitude,foo.latitude))
-    rowDict = {
-        'geometry': {'type':'LineString',
-            'coordinates':street},
-        'properties': {'Name': radialNames[idx],
+for name,r in radials.items():
+    if name == '3:00':
+        street = []
+        foo = geopy.distance.distance(feet=manRingRadius).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        foo = geopy.distance.distance(feet=streets['Esplanade']).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        rowDict = {
+            'geometry': {'type':'LineString',
+                'coordinates':street},
+            'properties': {'Name': name,
+                }
             }
-        }
-    lineShp.write(rowDict)
-    idx=idx+1
+        lineShp.write(rowDict)
+        # road from Center Camp Plaze camp to K
+    if name == '9:00':
+        street = []
+        foo = geopy.distance.distance(feet=manRingRadius).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        foo = geopy.distance.distance(feet=streets['Esplanade']).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        rowDict = {
+            'geometry': {'type':'LineString',
+                'coordinates':street},
+            'properties': {'Name': name,
+                }
+            }
+        lineShp.write(rowDict)
+        # road from Center Camp Plaze camp to K
+    if name == '6:00':
+        street = []
+        # special rules for the 6:00 radial
+        # road from Man Ring to Center Camp Plaze
+        foo = geopy.distance.distance(feet=manRingRadius).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        foo = geopy.distance.distance(feet=distToCenterCamp-centerCampPlazaRadius).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        rowDict = {
+            'geometry': {'type':'LineString',
+                'coordinates':street},
+            'properties': {'Name': name,
+                }
+            }
+        lineShp.write(rowDict)
+        # road from Center Camp Plaze camp to K
+        street = []
+        foo = geopy.distance.distance(feet=distToCenterCamp+centerCampPlazaRadius).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        foo = geopy.distance.distance(feet=streets['K']).destination(goldenSpike, bearing=r+degOffset)
+        street.append((foo.longitude,foo.latitude))
+        rowDict = ({
+            'geometry': {'type':'LineString',
+                'coordinates':street},
+            'properties': {'Name': name,
+                }
+            })
+        lineShp.write(rowDict)
+    else:
+        street = []
+        if r % 5 == 0:
+            foo = geopy.distance.distance(feet=streets['Esplanade']).destination(goldenSpike, bearing=r+degOffset)
+            street.append((foo.longitude,foo.latitude))
+            foo = geopy.distance.distance(feet=streets['K']).destination(goldenSpike, bearing=r+degOffset)
+            street.append((foo.longitude,foo.latitude))
+            rowDict = {
+                'geometry': {'type':'LineString',
+                    'coordinates':street},
+                'properties': {'Name': name,
+                    }
+                }
+            lineShp.write(rowDict)
+        else:
+            foo = geopy.distance.distance(feet=streets['F']).destination(goldenSpike, bearing=r+degOffset)
+            street.append((foo.longitude,foo.latitude))
+            foo = geopy.distance.distance(feet=streets['K']).destination(goldenSpike, bearing=r+degOffset)
+            street.append((foo.longitude,foo.latitude))
+            rowDict = {
+                'geometry': {'type':'LineString',
+                    'coordinates':street},
+                'properties': {'Name': name,
+                    }
+                }
+            lineShp.write(rowDict)
 
-# short radial streets at :15's
-idx=0
-shortRadial = degOffset+67.5
-while shortRadial < degOffset+300:
-    street = []
-    foo = geopy.distance.distance(feet=4590).destination(goldenSpike, bearing=shortRadial)
-    street.append((foo.longitude,foo.latitude))
-    foo = geopy.distance.distance(feet=maxDistance).destination(goldenSpike, bearing=shortRadial)
-    street.append((foo.longitude,foo.latitude))
-    rowDict = {
-        'geometry': {'type':'LineString',
-            'coordinates':street},
-        'properties': {'Name': shortRadialNames[idx],
-            }
+
+street = []
+# special rules for the 12:00 radial
+foo = geopy.distance.distance(feet=manRingRadius).destination(goldenSpike, bearing=degOffset)
+street.append((foo.longitude,foo.latitude))
+foo = geopy.distance.distance(feet=streets['Esplanade']-templeRingRadius).destination(goldenSpike, bearing=degOffset)
+street.append((foo.longitude,foo.latitude))
+rowDict = {
+    'geometry': {'type':'LineString',
+        'coordinates':street},
+    'properties': {'Name': '12:00',
         }
-    lineShp.write(rowDict)
-    shortRadial = shortRadial + 15
-    idx=idx+1
+    }
+lineShp.write(rowDict)
 
 # center camp ring
 manring = []
 ring = []
 ring2 = []
-for r in range(0,361,5):
-    foo = geopy.distance.distance(feet=250).destination(goldenSpike, bearing=r)
+templeRing = []
+radialThree = []
+radialNine = []
+for r in range((degOffset+0)*scale,(degOffset+360)*scale+1,1):
+    foo = geopy.distance.distance(feet=manRingRadius).destination(goldenSpike, bearing=r/scale)
     manring.append((foo.longitude,foo.latitude))
-    foo = geopy.distance.distance(feet=320).destination(centerCamp, bearing=r)
+    foo = geopy.distance.distance(feet=centerCampPlazaRadius).destination(centerCamp, bearing=r/scale)
     ring.append((foo.longitude,foo.latitude))
-    foo = geopy.distance.distance(feet=784).destination(centerCamp, bearing=r)
+    foo = geopy.distance.distance(feet=rodsRingRadius).destination(centerCamp, bearing=r)
     ring2.append((foo.longitude,foo.latitude))
+    foo = geopy.distance.distance(feet=templeRingRadius).destination(temple, bearing=r)
+    templeRing.append((foo.longitude,foo.latitude))
+    if r==90:
+        foo = geopy.distance.distance(feet=centerCampPlazaRadius).destination(centerCamp, bearing=r+degOffset)
+        radialThree.append((foo.longitude,foo.latitude))
+        foo = geopy.distance.distance(feet=streets['A']).destination(goldenSpike, bearing=radials['5:30']+degOffset)
+        radialThree.append((foo.longitude,foo.latitude))
+    if r==270:
+        foo = geopy.distance.distance(feet=centerCampPlazaRadius).destination(centerCamp, bearing=r+degOffset)
+        radialNine.append((foo.longitude,foo.latitude))
+        foo = geopy.distance.distance(feet=streets['A']).destination(goldenSpike, bearing=radials['6:30']+degOffset)
+        radialNine.append((foo.longitude,foo.latitude))
+        
 
 mrDict = {
   'geometry': {'type':'LineString',
@@ -117,4 +260,31 @@ rrDict = {
 }
 lineShp.write(rrDict)
 
+rrDict = {
+  'geometry': {'type':'LineString',
+      'coordinates':templeRing},
+  'properties': {'Name': 'Temple Ring Road',
+    }
+}
+lineShp.write(rrDict)
+
+rrDict = {
+  'geometry': {'type':'LineString',
+      'coordinates':radialThree},
+  'properties': {'Name': 'Center Camp 3:00 Road',
+    }
+}
+lineShp.write(rrDict)
+rrDict = {
+  'geometry': {'type':'LineString',
+      'coordinates':radialNine},
+  'properties': {'Name': 'Center Camp 9:00 Road',
+    }
+}
+lineShp.write(rrDict)
+
+
+
 lineShp.close()
+
+
